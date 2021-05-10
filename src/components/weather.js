3b96c538fd7c8d68e23de59bf28c16ef6041fc82
Mikko Lehtosaari <lehtiz@gmail.com>
2021-05-10 11:20:13 +0300

    Layout & cleanup

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,24 +4,31 @@ import './styles.css';
 import { format, fromUnixTime } from 'date-fns';
 import moment from 'moment';
 
-const CardExampleCard = ({ weatherData }) => (
+const Weather = ({ weatherData }) => (
   <div className="main">
     <div className="top">
-      <div className="header">City Name: {weatherData.name}</div>
-    </div>
-    <div className="flex">
-      <p className="date">Day: {moment().format('dddd')}</p>
-      <p className="date">Date: {moment().format('LL')}</p>
-      <p className="description">Description: {weatherData.weather[0].description}</p>
-    </div>
-    <div className="flex">
+      <div className="header">City Name: {weatherData.name} </div>
       <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
     </div>
-    <div className="flex sunrise-sunset">
+    <div className="dateContainer">
+      <div>
+        <p className="dateHeading">Day:</p>
+        <p className="dateItem">{moment().format('dddd')}</p>
+      </div>
+      <div>
+        <p className="dateHeading">Date:</p>
+        <p className="dateItem">{moment().format('LL')}</p>
+      </div>
+      <div>
+        <p className="dateHeading">Description:</p>
+        <p className="dateItem">{weatherData.weather[0].description}</p>
+      </div>
+    </div>
+    <div className="sunrise-sunset">
       <p>Sunrise: {format(fromUnixTime(weatherData.sys.sunrise), 'HH:mm:ss')}</p>
       <p>Sunset: {format(fromUnixTime(weatherData.sys.sunset), 'HH:mm:ss')}</p>
     </div>
   </div>
 );
 
-export default CardExampleCard;
+export default Weather;
